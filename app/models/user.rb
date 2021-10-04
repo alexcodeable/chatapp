39ef48b4b15369f770cc68e1ce64cf5f33a5b78a
alexcodeable <alexokomado@gmail.com>
2021-10-04 01:45:51 +0100

    added profile image
    Signed-off-by: alexcodeable <alexokomado@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :timeoutable, authentication_keys: [:login]
 
+    
+    has_one_attached :avatar
 
     has_many :messages
   
+    after_commit :add_default_avatar, on: %i[ create update ]
+
+    def avatar_thumbnail
+      if avatar.attached?
+      avatar.variant(resize: "150x150!").processed
+      else
+        "/default_avatar.jpg"
+      end
+    end
 
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }
@@ -25,4 +36,16 @@ class User < ApplicationRecord
         where(conditions.to_h).first
       end
     end
+
+
+  private
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join( 'app', 'assets', 'images', 'default_avatar.jpg')
+        ), filename: 'default_avatar.jpg', content_type: 'image/jpg'
+      )
+    end
+  end
 end
